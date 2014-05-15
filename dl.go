0d6dc36b74a111398e19cd8503c63199b438f916
jake <jake@coffshire.com>
2014-05-15 16:04:11 -0500

    init

---
+++ dl.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	items := []string{
+		"http://google.com",
+		"http://python.org",
+		"http://ruby-lang.org",
+		"http://golang.org",
+	}
+
+	c := make(chan string)
+
+	for _, item := range items {
+		go count(item, c)
+	}
+
+	timeout := time.After(500 * time.Second)
+	for i := 0; i < len(items); i++ {
+		select {
+		case result := <-c:
+			fmt.Print(result)
+		case <-timeout:
+			fmt.Print("Timed out\n")
+			return
+		}
+	}
+
+	fmt.Printf("Total time: %.2fs\n", time.Since(start).Seconds())
+}
+
+func count(url string, c chan<- string) {
+	start := time.Now()
+	r, err := http.Get(url)
+	if err != nil {
+		c <- fmt.Sprintf("%s: %s\n", url, err)
+		return
+	}
+	n, _ := io.Copy(ioutil.Discard, r.Body)
+	r.Body.Close()
+	dt := time.Since(start).Seconds()
+	c <- fmt.Sprintf("%s %d [%.2fs]\n", url, n, dt)
+}
---
+++ dl.py
@@ -0,0 +1,40 @@
+import multiprocessing
+import urllib2
+import Queue
+import time
+
+
+urls_to_load = [
+    "http://google.com",
+    "http://python.org",
+    "http://ruby-lang.org",
+    "http://golang.org",
+]
+
+limit = .5  # seconds
+
+
+def read_url(url, queue):
+    now = time.time()
+    data = urllib2.urlopen(url).read()
+    dt = time.time() - now
+    queue.put((url, len(data), dt))
+
+
+def fetch_parallel():
+    start = time.time()
+    queue = multiprocessing.Queue()
+    processes = [multiprocessing.Process(target=read_url, args=(url, queue)) for url in urls_to_load]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join(timeout=limit - (time.time() - start))
+        if p.is_alive():
+            p.terminate()
+        if not queue.empty():
+            print "%s %d [%.2fs]" % queue.get()
+    print "Total time: %.2fs" % (time.time() - start)
+
+
+if __name__ == "__main__":
+    fetch_parallel()
