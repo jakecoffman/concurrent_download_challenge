486103c7318e4467b3a85e1a14585865d78185ef
Jake Coffman <jake@jakecoffman.com>
2024-04-04 21:19:59 -0500

    modernize, switch to threads

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,6 @@
 import multiprocessing
-import urllib2
-import Queue
+import threading
+from urllib.request import urlopen
 import time
 
 
@@ -15,26 +15,24 @@ urls_to_load = [
 limit = .5  # seconds
 
 
-def read_url(url, queue):
+def read_url(url, q):
     now = time.time()
-    data = urllib2.urlopen(url).read()
+    data = urlopen(url).read()
     dt = time.time() - now
-    queue.put((url, len(data), dt))
+    q.put((url, len(data), dt))
 
 
 def fetch_parallel():
     start = time.time()
     queue = multiprocessing.Queue()
-    processes = [multiprocessing.Process(target=read_url, args=(url, queue)) for url in urls_to_load]
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join(timeout=limit - (time.time() - start))
-        if p.is_alive():
-            p.terminate()
-        if not queue.empty():
-            print "%s %d [%.4fs]" % queue.get()
-    print "Total time: %.4fs" % (time.time() - start)
+    threads = [threading.Thread(target=read_url, args=(url, queue)) for url in urls_to_load]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join(timeout=limit - (time.time() - start))
+    while not queue.empty():
+        print("%s %d [%.4fs]" % queue.get())
+    print("Total time: %.4fs" % (time.time() - start))
 
 
 if __name__ == "__main__":
